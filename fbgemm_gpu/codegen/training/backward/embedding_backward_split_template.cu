5a3a88efedb60c2d3cba5e319de2bd3443b4c4ad
Xinya Zhang <Xinya.Zhang@amd.com>
2024-07-17 23:19:56 -0500

    Fix the compiling error.

--- fbgemm_gpu/codegen/training/backward/embedding_backward_split_template.cu
+++ fbgemm_gpu/codegen/training/backward/embedding_backward_split_template.cu
@@ -22,6 +22,7 @@
 #include "fbgemm_gpu/split_embeddings_utils.cuh"
 #include "fbgemm_gpu/sparse_ops.h"
 #include "fbgemm_gpu/sparse_ops_utils.h"
+#include <iostream>
 
 using Tensor = at::Tensor;
 using namespace fbgemm_gpu;
